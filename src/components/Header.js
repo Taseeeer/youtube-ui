d608b905edb7bca0d4aa4b08b84ae1f550f5b36f
Syed Taseer Shah <syedtaseershah@Syeds-MacBook-Pro.local>
2022-04-17 17:21:24 +0500

    Youtube ui first commit

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import { AiFillYoutube, AiOutlineMenu, AiOutlineSearch, AiOutlineVideoCameraAdd, AiOutlineAppstore, AiOutlineBell } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
+import taseer from "../assets/cap.jpeg";
+
+export default function Header() {
+    return(
+        <header>
+            <div className="header-first">
+                <AiOutlineMenu />
+                <div className="logo">
+                    <AiFillYoutube className="y-logo" /> 
+                    <span className="youtube-written">Youtube</span>
+                    <span className="pk-written">PK</span>
+                </div>
+            </div>
+            <div className="header-second">
+                <form>
+                    <div className="input-background">
+                        <input placeholder="Search" />
+                    </div>
+                    <AiOutlineSearch className="header-search" />
+                    <BsFillMicFill className="header-mic" />
+                </form>
+            </div>
+            <div className="header-third">
+                <AiOutlineVideoCameraAdd />
+                <AiOutlineAppstore />
+                <AiOutlineBell />
+
+                <img className="user-image" src={taseer}  />
+            </div>
+        </header>
+    )
+}
